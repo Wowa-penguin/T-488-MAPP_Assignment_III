282316fc3852ffb34875097dc95e8154d5fe4708
Wowa_penguin <Heimir2710@gmail.com>
2025-12-08 12:59:19 +0000

    tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { store } from '@/store/index';
+import { Stack } from 'expo-router';
+import { Provider } from 'react-redux';
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+
+    );
 }
