3ef7740f2c451433f354a2e0118323e8b6ee26f7
Wowa_penguin <Heimir2710@gmail.com>
2025-12-09 11:00:29 +0000

    todo vinna á movie og cinema cards

---
+++ components/cinemaCard.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const CinemaCard = () => {
+    return (
+        <View>
+            <Text>Cinima</Text>
+        </View>
+    );
+};
+
+export default CinemaCard;
---
+++ components/movieCard.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const MovieCard = () => {
+    return (
+        <View>
+            <Text>Movie</Text>
+        </View>
+    );
+};
+
+export default MovieCard;
