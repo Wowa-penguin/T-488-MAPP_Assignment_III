065184dc5a01e16aa96aa754ae3676a3b8a6b9d6
Wowa_penguin <Heimir2710@gmail.com>
2025-12-09 11:43:29 +0000

    new cienma and movie details

---
+++ app/cinemas/cinemaDetails.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const CinemaDetails = () => {
+    return (
+        <View>
+            <Text>Cinema Details</Text>
+        </View>
+    );
+};
+
+export default CinemaDetails;
