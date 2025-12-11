ec2e26462d88ba40c273b28e6e8c48f8e42481c2
Wowa_penguin <Heimir2710@gmail.com>
2025-12-11 16:39:43 +0000

    remove index from movies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout() {
                 <Tabs.Screen
                     name="movies"
                     options={{
+                        href: null,
                         title: 'Movies',
                         headerShown: false,
                         tabBarIcon: ({ color }) => (
